1da3b7a15beca4ae68945cac045b5bb25a07dc3f
HornyVlad <vlad.kochergin.work@mail.ru>
2023-10-10 18:36:01 +0300

    final

--- app/src/main/java/com/example/vedroid1/main.java
+++ app/src/main/java/com/example/vedroid1/main.java
@@ -2,6 +2,8 @@ package com.example.vedroid1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,7 +13,7 @@ import androidx.annotation.Nullable;
 
 public class main extends Activity {
 
-    private int i = 0, j = 0;
+    private int i = 0, j = 0, k = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,22 +21,64 @@ public class main extends Activity {
         setContentView(R.layout.activity_helloact);
 
         Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        String text1 = "Левая ";
+        String text2 = "Правая ";
+
+
+        TextView mainTextLeft = findViewById(R.id.text1);
+        TextView mainTextRight = findViewById(R.id.text2);
+
+        mainTextLeft.setGravity(Gravity.RIGHT);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 i = i + 1;
-                TextView mainText = findViewById(R.id.text1);
-                mainText.setText("Левая:" + i);
+                mainTextLeft.setText(text1 + i);
+                mainTextRight.setText(text2 + j);
+                if(k%2 == 0 ) {
+                    mainTextLeft.setGravity(Gravity.RIGHT);
+                    mainTextRight.setGravity(Gravity.LEFT);
+                    int a = mainTextLeft.getGravity();
+                    int b = mainTextRight.getGravity();
+                    Log.i("AppLogger", "Left " + a);
+                    Log.i("AppLogger", "Right " + b);
+                }
+                else {
+                    mainTextLeft.setGravity(Gravity.RIGHT);
+                    mainTextRight.setGravity(Gravity.LEFT);
+                    int a = mainTextLeft.getGravity();
+                    int b = mainTextRight.getGravity();
+                    Log.i("AppLogger", "Left " + a);
+                    Log.i("AppLogger", "Right " + b);
+                }
+                k = k + 1;
             }
         });
 
-        Button button2 = findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 j = j + 1;
-                TextView mainText = findViewById(R.id.text2);
-                mainText.setText("Правая:" + j);
+                mainTextLeft.setText(text1 + i);
+                mainTextRight.setText(text2 + j);
+                if(k%2 == 0 ) {
+                    mainTextLeft.setGravity(Gravity.RIGHT);
+                    mainTextRight.setGravity(Gravity.LEFT);
+                    int a = mainTextLeft.getGravity();
+                    int b = mainTextRight.getGravity();
+                    Log.i("AppLogger", "Left " + a);
+                    Log.i("AppLogger", "Right " + b);
+                }
+                else {
+                    mainTextLeft.setGravity(Gravity.RIGHT);
+                    mainTextRight.setGravity(Gravity.LEFT);
+                    int a = mainTextLeft.getGravity();
+                    int b = mainTextRight.getGravity();
+                    Log.i("AppLogger", "Left " + a);
+                    Log.i("AppLogger", "Right " + b);
+                }
+                k = k + 1;
             }
         });
     }
